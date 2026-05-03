1c2de8d3df3be771eb281d15305a782452749fa6
Florent Clairambault <florent.clairambault@gmail.com>
2026-05-03 22:24:34 +0200

    feat(filestorage): add S3 backend (aws-sdk-go-v2)

---
+++ server/internal/handlers/filestorage/s3fs/s3fs_test.go
@@ -0,0 +1,31 @@
+package s3fs_test
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+
+	"github.com/fclairamb/solidping/server/internal/handlers/filestorage"
+	"github.com/fclairamb/solidping/server/internal/handlers/filestorage/s3fs"
+)
+
+func TestS3FS_ParseURI(t *testing.T) {
+	t.Parallel()
+
+	r := require.New(t)
+
+	b := s3fs.New("bucket", "prefix", nil)
+	orgUID := uuid.New()
+	fileID := uuid.New().String()
+	uri := "s3://" + orgUID.String() + "/reports/" + fileID
+
+	gotOrg, gotGroup, gotFile, err := b.ParseURI(uri)
+	r.NoError(err)
+	r.Equal(orgUID, gotOrg)
+	r.Equal(filestorage.GroupTypeReports, gotGroup)
+	r.Equal(fileID, gotFile)
+
+	_, _, _, err = b.ParseURI("file://" + orgUID.String() + "/reports/" + fileID)
+	r.ErrorIs(err, filestorage.ErrInvalidURI)
+}
