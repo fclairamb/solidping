eb32828786852a7fa52d431928b30f56b5796d2f
Florent Clairambault <florent.clairambault@gmail.com>
2026-08-29 20:05:37 +0200

    fix(dash0): default the organization section to Members, not Invitations

--- web/dash0/src/routes/orgs/$org/organization.index.tsx
+++ web/dash0/src/routes/orgs/$org/organization.index.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/orgs/$org/organization/")({
   beforeLoad: ({ params }) => {
     throw redirect({
-      to: "/orgs/$org/organization/invitations",
+      to: "/orgs/$org/organization/members",
       params: { org: params.org },
     });
   },
